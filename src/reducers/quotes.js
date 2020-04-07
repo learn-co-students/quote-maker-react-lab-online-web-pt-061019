37888d7ac589d982c7daff0d895b178288958423
vulcanism <cynomania@gmail.com>
2020-04-07 11:29:53 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,32 @@
 export default (state = [], action) => {
-  return state;
+  let index;
+  let quote;
+
+  switch (action.type) {
+
+    case "ADD_QUOTE":
+      return state.concat(action.quote);
+
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId)
+
+    case "UPVOTE_QUOTE":
+      quote = state.slice();
+      index = quote.findIndex(quote => quote.id === action.quoteId);
+
+      quote[index].votes += 1
+      return quote
+
+    case "DOWNVOTE_QUOTE":
+      quote = state.slice();
+      index = quote.findIndex(quote => quote.id === action.quoteId);
+
+      if (quote[index].votes !== 0) {
+        quote[index].votes -= 1
+      }
+      return quote
+
+    default:
+      return state;
+  }
 }
